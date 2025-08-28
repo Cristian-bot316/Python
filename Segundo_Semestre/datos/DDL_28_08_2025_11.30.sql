a47c5273d7c23c50131f382fe584814ace430263
Cristian-bot316 <taibo04d@gmail.com>
2025-08-28 12:27:32 -0400

    28/08/2025

--- Segundo_Semestre/datos/DDL_28_08_2025_11.30.sql
+++ Segundo_Semestre/datos/DDL_28_08_2025_11.30.sql
@@ -1,4 +1,4 @@
-CREATE TABLE nacionalidad(
+CREATE TABLE IF NOT EXISTS nacionalidad(
     id_nacionalidad INT AUTO_INCREMENT,
     pais VARCHAR(50) NOT NULL ,
     nacionalidad VARCHAR(50) NOT NULL,
@@ -7,3 +7,53 @@ CREATE TABLE nacionalidad(
 );
 
 INSERT INTO nacionalidad(pais,nacionalidad) VALUES('chile','chileno');
+
+
+CREATE TABLE IF NOT EXISTS autor(
+    id_autor INT AUTO_INCREMENT,
+    nombre_autor VARCHAR(100) NOT NULL,
+    pseudonimo VARCHAR(50) NULL,
+    id_nacionalidad INT NULL,
+    bio VARCHAR(255) NULL,
+
+
+    CONSTRAINT pk_autor PRIMARY KEY (id_autor),
+    CONSTRAINT fk_autor_nacionalidad FOREIGN KEY (id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS comuna(
+id_comuna INT AUTO_INCREMENT,
+codigo_comuna VARCHAR(5) NOT NULL,
+nombre_comuna VARCHAR(50) NOT NULL,
+
+CONSTRAINT pk_comuna PRIMARY KEY (id_comuna)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS direccion(
+    id_direccion INT AUTO_INCREMENT,
+    id_comuna INT NOT NULL,
+    calle VARCHAR(50) NOT NULL,
+    numero VARCHAR(10) NULL,
+    departamento VARCHAR(10) NULL,
+    
+    
+    CONSTRAINT pk_direccion PRIMARY KEY (id_direccion),
+    CONSTRAINT FK_direccion_comuna FOREIGN KEY (id_comuna) REFERENCES comuna (id_comuna)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
