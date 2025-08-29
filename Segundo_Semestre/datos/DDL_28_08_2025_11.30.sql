f6e9e32cae5d0df576a1c2e6d1ddf19eb44b25ae
Cristian-bot316 <taibo04d@gmail.com>
2025-08-29 18:27:21 -0400

    29-08-2025

--- Segundo_Semestre/datos/DDL_28_08_2025_11.30.sql
+++ Segundo_Semestre/datos/DDL_28_08_2025_11.30.sql
@@ -34,26 +34,67 @@ CONSTRAINT pk_comuna PRIMARY KEY (id_comuna)
 
 
 CREATE TABLE IF NOT EXISTS direccion(
-    id_direccion INT AUTO_INCREMENT,
+    id_direccion INT 
     id_comuna INT NOT NULL,
     calle VARCHAR(50) NOT NULL,
     numero VARCHAR(10) NULL,
     departamento VARCHAR(10) NULL,
-    
+    AUTO_INCREMENT,
     
     CONSTRAINT pk_direccion PRIMARY KEY (id_direccion),
     CONSTRAINT FK_direccion_comuna FOREIGN KEY (id_comuna) REFERENCES comuna (id_comuna)
 );
 
 
+CREATE TABLE IF NOT EXISTS biblioteca(
+    id_biblioteca INT AUTO_INCREMENT,
+
+
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS lector(
+    id_lector INT AUTO_INCREMENT,
+
+);
+
+
+CREATE TABLE IF NOT EXISTS categoria(
+    id_categoria INT AUTO_INCREMENT,
+
+
+);
 
 
 
 
 
+CREATE TABLE IF NOT EXISTS libro(
+    id_libro INT AUTO_INCREMENT,
+    id_biblioteca INT NOT NULL,
+    id_autor INT NOT NULL,
+    id_categoria INT NULL,
+
+
+);
 
 
 
+CREATE TABLE IF NOT EXISTS prestamo (
+    id_prestamo INT AUTO_INCREMENT,
+    id_libro INT NOT NULL,
+    rut_lector INT NOT NULL,
+    fecha_prestamo DATETIME NOT NULL,
+    fecha_devolucion DATETIME NOT NULL,
+    fecha_entrega DATETIME NOT NULL,
+
+    CONSTRAINT pk_prestamo PRIMARY KEY (id_prestamo),
+    CONSTRAINT fk_prestamo_libro FOREIGN KEY (id_libro) REFERENCES libro (id_libro,),
+    CONSTRAINT fk_prestamo_lector FOREIGN KEY (rut_lector) REFERENCES lector (rut_lector,)
 
 
+
+);
 
